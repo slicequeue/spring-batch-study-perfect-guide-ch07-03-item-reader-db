d4c35276365c57cd24bdb71c00052086465aa212
slicequeue <slicequeue@gmail.com>
2024-04-16 00:12:28 +0900

    feat: 도메인 클래스 Customer 선언 & 로우 맵퍼 구현

---
+++ src/main/java/com/slicequeue/springboot/batch/jdbc/domain/Customer.java
@@ -0,0 +1,94 @@
+package com.slicequeue.springboot.batch.jdbc.domain;
+
+public class Customer {
+
+    private Long id;
+
+    private String firstName;
+    private String middleInitial;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private String zipCode;
+
+    public Customer() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleInitial() {
+        return middleInitial;
+    }
+
+    public void setMiddleInitial(String middleInitial) {
+        this.middleInitial = middleInitial;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", middleInitial='" + middleInitial + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zipCode='" + zipCode + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/slicequeue/springboot/batch/jdbc/mapper/CustomerRowMapper.java
@@ -0,0 +1,27 @@
+package com.slicequeue.springboot.batch.jdbc.mapper;
+
+import com.slicequeue.springboot.batch.jdbc.domain.Customer;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CustomerRowMapper implements RowMapper<Customer> {
+
+    @Override
+    public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Customer customer = new Customer();
+
+        customer.setId(rs.getLong("id"));
+        customer.setAddress(rs.getString("address"));
+        customer.setCity(rs.getString("city"));
+        customer.setFirstName(rs.getString("firstName"));
+        customer.setMiddleInitial(rs.getString("middleInitial"));
+        customer.setLastName(rs.getString("lastName"));
+        customer.setState(rs.getString("state"));
+        customer.setZipCode(rs.getString("zipCode"));
+
+        return customer;
+    }
+
+}
